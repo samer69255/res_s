a6cfaefa491a492810ba40fb660b00d39c5722f6
zaincashu <zaincashu@gmail.com>
2018-09-24 20:43:11 +0300

    '#1'

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ app.post('/webhook/', function (req, res) {
                     fb.sendTextMessageT(sender,2);
                 }
             else {
-                fb.sendFileMessage(sender,'...')
+                fb.sendTextMessage(sender,'...')
             }
             
             continue;
