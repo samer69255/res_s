35a2818d6f92f2b39ccb73533410d109b1819cb7
zaincashu <zaincashu@gmail.com>
2018-09-24 18:51:47 +0300

    '#1'

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ app.post('/webhook/', function (req, res) {
             }
             
             
-            fb.sendTextMessage(sender,text);
+            fb.sendTextMessageT(sender,text);
             
             
 
